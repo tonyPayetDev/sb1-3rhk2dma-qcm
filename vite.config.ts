7d7ad3b08217313f2736f416da9f5848a4928a21
tony <tony.payet.professionnel@gmail.com>
2025-02-18 10:49:39 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000',  // Proxy pour rediriger les requêtes vers le backend
+    },
+  },
   optimizeDeps: {
     include: ['react', 'react-dom'],
     exclude: ['@remotion/player', 'remotion'],
